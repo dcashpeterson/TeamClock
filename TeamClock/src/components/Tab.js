48c9f4120495dcd7bb137c9297351c3d196ce47f
Bob German <bgerman1@hotmail.com>
2020-08-12 16:46:41 -0400

    adal test bed works for web and tab

--- TeamClock/src/components/Tab.js
+++ TeamClock/src/components/Tab.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as microsoftTeams from "@microsoft/teams-js";
 import TeamService from '../services/TeamService';
+import * as MicrosoftGraphClient from "@microsoft/microsoft-graph-client";
 
 import Clock from './Clock';
 
@@ -12,7 +13,9 @@ class Tab extends React.Component {
     super(props);
     this.state = {
       context: {},
-      teamService: null
+      teamService: null,
+      accessToken: '',
+      messages: []
     }
   }
 
@@ -24,29 +27,106 @@ class Tab extends React.Component {
           this.setState({
             context: context
           });
+          // Get the Team Service and set it in the state
+          TeamService.factory(context).then((service) => {
+            this.setState({
+              teamService: service
+            });
+          });
+          // Set up the Graph service
+          this.msGraphClient = MicrosoftGraphClient.Client.init({
+            authProvider: async (done) => {
+              if (!this.state.accessToken) {
+                const token = await this.getAccessToken();
+                this.setState({
+                  accessToken: token
+                });
+              }
+              done(null, this.state.accessToken);
+            }
+          });
         }
       });
     }
-    // Get the Team Service and set it in the state
-    TeamService.factory().then((service) => {
-      this.setState({
-        teamService: service
-      });  
-    });
   }
 
   render() {
 
     if (this.state.context && this.state.teamService) {
-    // let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
-    return (
-      <Clock teamService={this.state.teamService} />
-    );
+      // let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
+      return (
+        <div>
+          <Clock teamService={this.state.teamService} />
+          <button onClick={this.handleGetMyMessagesOnClick}>Get My Messages</button>
+          {this.state.messages.map(message => (
+            <p>
+              {message.receivedDateTime} --- {message.subject}
+            </p>
+          ))
+          }
+        </div>
+      );
     } else {
       return null;
     }
 
   }
+
+  /************* FROM ADAL SAMPLE *******************/
+  handleGetMyMessagesOnClick = async (event) => {
+    await this.getMessages();
+  }
+
+  async getMessages(promptConsent = false) {
+    if (promptConsent || this.state.accessToken === "") {
+      await this.signin(promptConsent);
+    }
+
+    this.msGraphClient
+      .api("me/mailFolders/inbox/messages")
+      .select(["receivedDateTime", "subject"])
+      .top(15)
+      .get(async (error, rawMessages, rawResponse) => {
+        if (!error) {
+          this.setState(Object.assign({}, this.state, {
+            messages: rawMessages.value
+          }));
+          Promise.resolve();
+        } else {
+          console.error("graph error", error);
+          // re-sign in but this time force consent
+          await this.getMessages(true);
+        }
+      });
+  }
+
+  async signin(promptConsent = false) {
+    const token = await this.getAccessToken(promptConsent);
+
+    this.setState({
+      accessToken: token
+    });
+
+    Promise.resolve();
+  }
+
+  async getAccessToken(promptConsent = false) {
+    return new Promise((resolve, reject) => {
+      microsoftTeams.authentication.authenticate({
+        url: window.location.origin + "/TeamClock/auth/teams-adal/auth-start.html",
+        width: 600,
+        height: 535,
+        successCallback: (accessToken) => {
+          resolve(accessToken);
+        },
+        failureCallback: (reason) => {
+          reject(reason);
+        }
+      });
+    });
+  }
+
+  /************** END FROM ADAL SAMPLE ************/
 
 }
 export default Tab;
--- TeamClock/src/components/Web.js
+++ TeamClock/src/components/Web.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TeamService from '../services/TeamService';
+import * as MicrosoftGraphClient from "@microsoft/microsoft-graph-client";
 
 import Clock from './Clock';
 
@@ -9,16 +10,34 @@ import Clock from './Clock';
 class Web extends React.Component {
   constructor(props) {
     super(props);
+    let token = '';
+    let s = window.location.hash.indexOf('t=');
+    if (s > 0) token = window.location.hash.substring(s+2);
     this.state = {
-      teamService: null
+      teamService: null,
+      accessToken: token,
+      messages: []
     }
   }
 
   componentDidMount() {
+    // Get the Team Service and set it in the state
     TeamService.factory().then((service) => {
       this.setState({
         teamService: service
       });
+    });
+    // Set up the Graph service
+    this.msGraphClient = MicrosoftGraphClient.Client.init({
+      authProvider: async (done) => {
+        if (!this.state.accessToken) {
+          const token = await this.getAccessToken();
+          this.setState({
+            accessToken: token
+          });
+        }
+        done(null, this.state.accessToken);
+      }
     });
   }
 
@@ -28,6 +47,13 @@ class Web extends React.Component {
         <div>
           <h1>Team clock</h1>
           <Clock teamService={this.state.teamService} />
+          <button onClick={this.handleGetMyMessagesOnClick}>Get My Messages</button>
+          {this.state.messages.map(message => (
+            <p>
+              {message.receivedDateTime} --- {message.subject}
+            </p>
+          ))
+          }
         </div>
       );
     } else {
@@ -35,6 +61,53 @@ class Web extends React.Component {
     }
 
   }
+  /************* FROM ADAL SAMPLE *******************/
+  handleGetMyMessagesOnClick = async (event) => {
+    await this.getMessages();
+  }
 
+  async getMessages(promptConsent = false) {
+    if (promptConsent || this.state.accessToken === "") {
+      await this.signin(promptConsent);
+    }
+
+    this.msGraphClient
+      .api("me/mailFolders/inbox/messages")
+      .select(["receivedDateTime", "subject"])
+      .top(15)
+      .get(async (error, rawMessages, rawResponse) => {
+        if (!error) {
+          this.setState(Object.assign({}, this.state, {
+            messages: rawMessages.value
+          }));
+          Promise.resolve();
+        } else {
+          console.error("graph error", error);
+          // re-sign in but this time force consent
+          await this.getMessages(true);
+        }
+      });
+  }
+
+  async signin(promptConsent = false) {
+    const token = await this.getAccessToken(promptConsent);
+
+    this.setState({
+      accessToken: token
+    });
+
+    Promise.resolve();
+  }
+
+  async getAccessToken(promptConsent = false) {
+    return new Promise((resolve, reject) => {
+      window.location.replace(
+        window.location.origin + "/TeamClock/auth/web-adal/auth-start.html"
+      )
+      // We never return
+    });
+  }
+
+  /************** END FROM ADAL SAMPLE ************/
 }
 export default Web;
